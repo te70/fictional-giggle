62cc82f13d2e176fc9a28b8017c8a099a6d712e7
te70 <tevinkinuthia@gmail.com>
2024-02-05 17:07:06 +0300

    Add router files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import './App.css';
-import Companies from './components/Companies/Companies';
-import Residencies from './components/Residencies/Residencies';
 import Value from './components/Value/Value';
 import Contact from './components/Contact/Contact';
 import GetStarted from './components/GetStarted/GetStarted';
 import Footer from './components/Footer/Footer';
 import What from './components/What/What';
 import How from './components/How/How';
-import Why from './components/Why/Why';
 import Cor from './components/Cor/Cor';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Form from './components/Form/Form';
 function App() {
   return (
-    <div className="App">
-      <div>
-        <div className="white-gradient"/>
-        <Header/>
-        <Hero/>
+    <Router>
+      <div className="App">
+        <div>
+          <div className="white-gradient"/>
+          <Header/>
+          <Hero/>
+        </div>
+        <What/>
+        <How/>
+        <Cor/>
+        <Value/>
+        <Contact/>
+        <GetStarted/>
+        <Footer/>
+        {/* <Switch>
+          <Route path='/submit-cv' component={Form}/>
+        </Switch> */}
       </div>
-      {/* <Companies/> */}
-      <What/>
-      <How/>
-      <Cor/>
-      {/* <Residencies/> */}
-      <Value/>
-      <Contact/>
-      <GetStarted/>
-      <Footer/>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <section className='r-wrapper'>
+      <div className='paddings innerWidth r-container'>
+        <div className="r-head flexColStart">
+          <span className='primaryText'>How we do it</span>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Form
