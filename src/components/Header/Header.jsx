95771bae129b8eeebc04450e414d7f8e431ca534
te70 <tevinkinuthia@gmail.com>
2024-02-05 15:03:15 +0300

    Update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,10 +18,9 @@ function Header() {
 
             <OutsideClickHandler onOutsideClick={()=>setMenuOpened(false)}>
             <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
-                <a href="mailto:info@elvich.co.ke">info@elvich.co.ke</a>
-                <a href="#value">Jobs</a>
                 <a href="#value">Our Value</a>
-                <a href="#contact">Contact</a>
+                <a href="#value">Jobs</a>
+                <a href="mailto:info@elvich.co.ke">info@elvich.co.ke</a>
                 <button className="button">
                     <a href="">Submit cv</a>
                 </button> 
